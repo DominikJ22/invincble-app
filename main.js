24c910be5d3e0fd2ecd00e4ffcb7623688525ac4
Dominik Jones <dominikjo22@gmail.com>
2022-11-16 18:40:05 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+document.querySelector('#getButton').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const charName = document.querySelector('input').value
+    try{
+        const response = await fetch( `https://incinvible.cyclic.app/api/${charName}`)
+        const data = await response.json()
+        console.log(data)
+        
+        document.getElementById('charName').innerText = data.alienName
+        document.getElementById('charSpecies').innerText = data.speciesName
+        document.getElementById('charWorld').innerText = data.homeWorld
+        document.getElementById('charTeam').innerText = data.Team
+        document.getElementById('charSuit').innerText = data.Suit
+        document.getElementById('charAbilities').innerText = data.
+        Abilities
+        document.getElementById('alienImage').src = data.image
+       
+    }catch(error) {
+        console.log(error)
+    }
+}
