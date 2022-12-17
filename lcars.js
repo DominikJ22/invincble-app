db25b6cd6bc13e13f9e174528c05c45f99a1656c
Dominik Jones <dominikjo22@gmail.com>
2022-12-16 23:55:16 -0500

    update servers

--- lcars.js
+++ lcars.js
@@ -3,7 +3,7 @@ document.getElementById('getButton').addEventListener('click', apiRequest)
 async function apiRequest(){
     const alienName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://star-trek-api.herokuapp.com/api/${alienName}`)
+        const response = await fetch(`/https://puce-powerful-trout.cyclic.app/${alienName}`)
         const data = await response.json()
 
         console.log(data)
