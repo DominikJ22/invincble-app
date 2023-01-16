49c58219739acb8c539dd3d0d59eebdad6879a6a
Dominik Jones <dominikjo22@gmail.com>
2023-01-16 16:51:21 -0500

    final fix

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.querySelector('#getButton').addEventListener('click', apiRequest)
 async function apiRequest(){
     const alienName = document.querySelector('input').value
     try{
-        const response = await fetch( `https://invincible-o7rr.onrender.com//api/${alienName}`)
+        const response = await fetch( `https://invincible-o7rr.onrender.com/api/${alienName}`)
         const data = await response.json()
         console.log(data)
         
@@ -14,7 +14,7 @@ async function apiRequest(){
         document.getElementById('alienSuit').innerText = data.Suit
         document.getElementById('alienAbilities').innerText = data.
         Abilities
-        document.getElementById('alienImage').src = data.image
+       document.getElementById('alienImage').src = data.image
        
     }catch(error) {
         console.log(error)
