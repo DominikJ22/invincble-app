cff6364cce24b6b9b8104a212e800355bcb3955c
Dominik Jones <dominikjo22@gmail.com>
2023-01-16 17:04:57 -0500

    fixes

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ async function apiRequest(){
         document.getElementById('alienAbilities').innerText = data.
         Abilities
        document.getElementById('alienImage').src = data.image
-       document.getElementById('alienCaption').innerText = data.speciesName
        
     }catch(error) {
         console.log(error)
